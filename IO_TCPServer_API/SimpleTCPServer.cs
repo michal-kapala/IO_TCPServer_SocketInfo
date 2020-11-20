6d91c0432516a94b4e503e72a205793060fa3260
Bartosz Ciesielczyk <purpleturtll@gmail.com>
2020-11-20 20:16:40 +0100

    Simple broadcasting of messages

--- IO_TCPServer_API/SimpleTCPServer.cs
+++ IO_TCPServer_API/SimpleTCPServer.cs
@@ -3,6 +3,7 @@ using System.Net.Sockets;
 using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 
 namespace IO_TCPServer_API
 {
@@ -12,11 +13,23 @@ namespace IO_TCPServer_API
         byte[] buffer;
         const int bufferSize = 1024;
         public delegate void TransmissionDelegate(TcpClient client);
+        List<String> messages;
+        Dictionary<int, TcpClient> clients;
 
         public SimpleTCPServer(string ip, ushort port, uint bufferSize)
         {
             listener = new TcpListener(IPAddress.Parse(ip), port);
             buffer = new byte[bufferSize];
+            messages = new List<String>();
+            clients = new Dictionary<int, TcpClient>();
+        }
+
+        private void BroadcastMessage(String message)
+        {
+            foreach (KeyValuePair<int, TcpClient> client in clients){
+                byte[] buffer = System.Text.Encoding.Unicode.GetBytes(message);
+                client.Value.GetStream().Write(buffer, 0, buffer.Length);
+            }
         }
 
         public void SendData(TcpClient client)
@@ -31,7 +44,7 @@ namespace IO_TCPServer_API
                     string command = System.Text.Encoding.UTF8.GetString(buffer, 0, dataSize);
                     Regex sanitize = new Regex("[^a-zA-Z0-9#]");
                     command = sanitize.Replace(command, "");
-                    TextProtocol.Process(client, command);
+                    BroadcastMessage(command);
                     if (command == "#disconnect") break;
                 }
             }
@@ -56,6 +69,7 @@ namespace IO_TCPServer_API
             while (true)
             {
                 TcpClient client = listener.AcceptTcpClient();
+                clients.Add(client.GetHashCode(), client);
                 ConsoleLogger.Log("New connection established: " + TextProtocol.GetSocketInfo(client, false), LogSource.SERVER);
                 TransmissionDelegate transDelegate = new TransmissionDelegate(SendData);
                 transDelegate.BeginInvoke(client, TransmissionCallbackStub, client);
