630d36e4771b5cb30ea32e01c03bff6df6650bd9
Bartosz Ciesielczyk <purpleturtll@gmail.com>
2020-11-20 20:52:31 +0100

    dumb scrren clear and sanitizer accepting space

--- IO_TCPServer_API/SimpleTCPServer.cs
+++ IO_TCPServer_API/SimpleTCPServer.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.IO;
 using System.Text.RegularExpressions;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace IO_TCPServer_API
 {
@@ -13,22 +14,27 @@ namespace IO_TCPServer_API
         byte[] buffer;
         const int bufferSize = 1024;
         public delegate void TransmissionDelegate(TcpClient client);
-        List<String> messages;
+        List<string> messages;
         Dictionary<int, TcpClient> clients;
 
         public SimpleTCPServer(string ip, ushort port, uint bufferSize)
         {
             listener = new TcpListener(IPAddress.Parse(ip), port);
             buffer = new byte[bufferSize];
-            messages = new List<String>();
+            messages = new List<string>();
             clients = new Dictionary<int, TcpClient>();
         }
 
-        private void BroadcastMessage(String message)
+        private void BroadcastMessages()
         {
+            string clear = String.Concat(Enumerable.Repeat("\n", 100));
             foreach (KeyValuePair<int, TcpClient> client in clients){
-                byte[] buffer = System.Text.Encoding.Unicode.GetBytes(message);
-                client.Value.GetStream().Write(buffer, 0, buffer.Length);
+                client.Value.GetStream().Write(System.Text.Encoding.Unicode.GetBytes(clear), 0, clear.Length);
+                foreach (string message in messages)
+                {
+                    byte[] buffer = System.Text.Encoding.Unicode.GetBytes(message + "\n");
+                    client.Value.GetStream().Write(buffer, 0, buffer.Length);
+                }
             }
         }
 
@@ -42,9 +48,10 @@ namespace IO_TCPServer_API
                 while ((dataSize = stream.Read(buffer, 0, buffer.Length)) != 0)
                 {
                     string command = System.Text.Encoding.UTF8.GetString(buffer, 0, dataSize);
-                    Regex sanitize = new Regex("[^a-zA-Z0-9#]");
+                    Regex sanitize = new Regex("[^a-zA-Z0-9#(+ +)]");
                     command = sanitize.Replace(command, "");
-                    BroadcastMessage(command);
+                    messages.Add(command);
+                    BroadcastMessages();
                     if (command == "#disconnect") break;
                 }
             }
